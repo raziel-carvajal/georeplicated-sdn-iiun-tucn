e8a6bee8e89da4422fda46801ecc83262e6be036
raziel-carvajal <raziel.carvajal@unine.ch>
2017-02-17 11:34:34 +0100

    Configuration files where added

--- copyAtController.sh
+++ copyAtController.sh
@@ -21,5 +21,6 @@ set -o nounset                              # Treat unset variables as an error
 
 files="deploy-net-monitor.sh linksNetTool mapCloudAp monitor-links.sh"
 files=${files}" mapNetTool linksCloudAp killNetTool.sh parse-atr-logs.sh"
-files=${files}" parse-owd-logs.sh"
+files=${files}" parse-owd-logs.sh deployISPN.sh distributedCache.xml ycsbCacheCfg"
+files=${files}" ycsbBenchCfg"
 scp ${files} dionasys-controller:~/georeplicated-sdn-iiun-tucn
