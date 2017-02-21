15a5ccd0d4969f8995c083475ecba7bd0a73aa2c
Raziel Carvajal-Gomez <raziel.carvajal-gomez@inria.fr>
2017-02-21 23:20:07 +0100

    This script place the datasets of OWD/ATR and one cloud application (ZK or ISPN) in the directory where our experiments are stored

--- fetchBaseline.sh
+++ fetchBaseline.sh
@@ -21,8 +21,49 @@ set -o nounset                              # Treat unset variables as an error
 
 method=$1
 cloudA=$2
+remFil=$3 #<< /PATH/file.ext
+dstDir=`dirname ${4}`
 
-if [ ${#} -lt 2 ] ; then
+if [ ${#} -lt 4 ] ; then
   exit 1
 fi
 
+#TODO VERIFY STRINGS (VERY IMPORTANT)
+#TODO CHECK IF DESTINATION DIRECTORY EXISTS
+#XXX take into consideration that this tar file contains three datasets: OWD/ATR/(ZK | ISPN)
+
+rm -fr tmp ; mkdir tmp ; cd tmp ; origin=`pwd` 
+#TODO CHECK IF REMOTE FILE WITH FULL PATH IS PRESENT
+scp dionasys-controller:${remFil} .
+tarFi=`basename ${remFil}`
+tar xof ${tarFi}
+tgetDir=${dstDir}/${method}
+mv ${tarFi} ${tgetDir}
+
+i=$((${#tarFi}-4))
+diName=${tar:0:${i}}
+
+tmp=${tgetDir}/atr/${diName}
+mkdir ${tmp}
+mv ${diName}/atr/*.parAtr ${tmp}
+cd ${tgetDir}/atr ; rm -f current
+ln -s ${diName} current
+
+cd ${origin}
+
+tmp=${tgetDir}/owd/${diName}
+mkdir ${tmp}
+mv ${diName}/owd/*.parOwd ${tmp}
+cd ${tgetDir}/owd ; rm -f current
+ln -s ${diName} current
+
+cd ${origin}/${diName}
+tar xof ${cloudA}.tgz
+
+tmp=${tgetDir}/${cloudA}/${diName}
+mkdir ${tmp}
+mv ${cloudA}/*.out ${tmp}
+cd ${tgetDir}/${cloudA} ; rm -f current
+ln -s ${diName} current
+
+cd ${origin} ; cd .. ; rm -fr tmp
