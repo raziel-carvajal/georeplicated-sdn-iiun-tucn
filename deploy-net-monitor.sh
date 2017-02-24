9bdfd2c831f24ed0bc34d7e3d9fa8f201c7a89b6
Raziel Carvajal-Gomez <raziel.carvajal-gomez@inria.fr>
2017-02-24 11:53:30 +0100

    Stable version of measuring OWD/ATR

--- deploy-net-monitor.sh
+++ deploy-net-monitor.sh
@@ -18,7 +18,7 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-
+MATHLAB_TIMEOUT=10
 if [ ${#} -lt 1 ] ; then
   echo "USAGE: $0 [Number of minutes to let ATR and OWD be measured]"
   exit 1
@@ -28,33 +28,43 @@ logs="tucn-logs"
 rm -fr ${logs} ; mkdir ${logs}
 
 sitesNo=`cat mapNetTool | wc -l`
-echo "Deploying TUCN tool..."
+echo "Deploying TUCN tool"
+cmdAtr="cd /usr/local/src/ATRAM"
+cmdOwd="cd /usr/local/src"
 for (( CNTR=1; CNTR<=${sitesNo}; CNTR+=1 )); do
   site=`cat mapNetTool | head -${CNTR} | tail -1 | awk '{print $1}'`
-  cmd="cd /usr/local/src/ATRAM"
   if [ "${site}" == "clu" ]; then
-      echo "Launching NetServer in site ${site}..."
-      ssh ${site}-nt "${cmd} ; ./netserver.sh &>netServer.log &"
+      echo "Launching NetServer in site: ${site}"
+      ssh ${site}-nt "${cmdAtr} ; ./netserver.sh &>${site}-netServer.log &"
+      echo -e "\tLaunching process to measure OWD"
+      ssh ${site}-nt "${cmdOwd} ; ./matlab_script.sh &>${site}-mathLab.log &"
+      #timeout for waiting MathLab to be launched
+      sleep ${MATHLAB_TIMEOUT}
+      echo -e "\t\tDONE\n\tDONE"
   else
-      echo "Launching NetPerf in site ${site}..."
-      ssh ${site}-nt "${cmd} ; ./netperf.sh &>netPerf.log &"
+      echo "Launching NetPerf in site: ${site}"
+      ssh ${site}-nt "${cmdAtr} ; ./netperf.sh &>${site}-netPerf.log &"
+      echo -e "\tDONE"
   fi
+  echo "Launching process to measure OWD in site: ${site}"
+  ssh ${site}-nt "${cmdOwd} ; ./multicastOWD -p 5000 &>${site}-owd.log &"
   echo -e "\tDONE"
 done
 
-echo -e "Waiting until timeout expires..."
+echo -e "Waiting until timeout expires"
 let tInSec=${timeout}*60
 sleep ${tInSec}
 echo -e "\tDONE\nLaunching STOP script in all nodes"
 
 # Not in background to stop every process
-cmd="cd /usr/local/src/ATRAM ; ./stop.sh &>stop.log"
 for (( CNTR=1; CNTR<=${sitesNo}; CNTR+=1 )); do
   site=`cat mapNetTool | head -${CNTR} | tail -1 | awk '{print $1}'`
-  echo "Doing STOP in site ${site}..."
-  ssh ${site}-nt "${cmd}" 
+  echo "Doing STOP of OWD/ATR measurements in site ${site}..."
+  ssh ${site}-nt "${cmdAtr} ; ./stop.sh &>${site}-atr-stop.log" 
+  ssh ${site}-nt "${cmdOwd} ; ./stopOWD.sh &>${site}-owd-stop.log" 
   echo -e "\tDONE\nFetching logs from site ${site}..."
   scp ${site}-nt:/usr/local/src/ATRAM/*.log ${logs}/
+  scp ${site}-nt:/usr/local/src/*.log ${logs}/
   echo -e "\tDONE"
 done
 
